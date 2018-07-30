5457062123f83fa0d1ef687193369d23da230aad
bitstacker <bitstacker@never-afk.de>
2018-07-30 17:52:34 +0200

    experiments

---
+++ test.py
@@ -0,0 +1,23 @@
+import serial
+import binascii
+from PyCRC.CRCCCITT import CRCCCITT
+
+SMLSTART = bytes([0x1B, 0x1B, 0x1B, 0x1B, 0x01, 0x01, 0x01, 0x01])
+SMLSTOP = bytes([0x1B, 0x1B, 0x1B, 0x1B, 0x1A])
+index = 0
+reading = True
+data = SMLSTART
+ser = serial.Serial('/dev/ttyUSB0')
+if(ser.read_until(SMLSTART, 800)):
+    while(reading):
+        b = ser.read(1)
+        data = data + b
+        if b == SMLSTOP[index:index+1]:
+            index = index + 1
+        if index >= 5:
+          data = data + ser.read(3)
+          reading = False
+ser.close() 
+print(hex(CRCCCITT().calculate(data[0:len(data)-2])))
+print(data[0:len(data)-2].hex())
+print(data[len(data)-2:].hex())
